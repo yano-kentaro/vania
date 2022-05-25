b13ff175b85f561149a96e964c4ed66e043210a4
yano-kentaro <ynkn0829@gmail.com>
2022-05-25 23:12:19 +0900

    tsxテンプレート更新

--- client/src/components/landing/SignUpDialog.tsx
+++ client/src/components/landing/SignUpDialog.tsx
@@ -1,4 +1,4 @@
-//┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 //┃
 //┃──┨ SignUpDialog.tsx [Ver.2022_05_23] ┃
 //┃
@@ -8,39 +8,34 @@
 //┃
 //┃──┨ ランディングページのサインアップダイアログ ┃
 //┃
-//┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-//===================================================|0
+//=============================================================|0
 //                    依存関係
-//==========================================|2022_05_23
+//====================================================|2022_05_23
 
-//------------------------------
+//------------------------------------------
 // Modules
-// import { useState } from 'react'
-import { atom, selector, useRecoilState } from 'recoil'
-import { getLang } from '../../lib/common'
-import {
-  dialogTitle, dialogText, cancelLabel, okLabel
-} from '../../msg/landing/signUpDialog'
+import * as RCL from 'recoil'
 
-//------------------------------
-// components
-import {
-  Dialog as MUIDialog,
-  DialogTitle as MUIDialogTitle,
-  DialogContent as MUIDialogContent,
-  DialogContentText as MUIDialogContentText,
-  DialogActions as MUIDialogActions,
-  Button as MUIButton,
-  TextField as MUITextField
-} from '@mui/material'
+//------------------------------------------
+// Components
+import * as MUI from '@mui/material'
 
-//------------------------------
-// static
+//------------------------------------------
+// Functions
+import * as COMMON from '../../lib/common'
 
-//===================================================|0
+//------------------------------------------
+// Text messages
+import * as MSG from '../../msg/landing/signUpDialog'
+
+//------------------------------------------
+// Static
+
+//=============================================================|0
 //                    スタイル定義
-//==========================================|2022_05_25
+//====================================================|2022_05_25
 
 const styleContentText = {
   "white-space": "pre-wrap"
@@ -51,47 +46,54 @@ const styleTextField = {
   "width": "90%"
 }
 
-//===================================================|0
+//=============================================================|0
 //                    状態管理定義
-//==========================================|2022_05_25
+//====================================================|2022_05_25
 
-//------------------------------
+//------------------------------------------
 // ダイアログフラグ
-const signUpFlagState = atom({
+const signUpFlagState = RCL.atom({
   key: 'signUpFlagState',
   default: false
 })
 
-//===================================================|0
+//=============================================================|0
 //                    コンポーネント定義
-//==========================================|2022_05_23
+//====================================================|2022_05_23
 function SignUpDialog() {
-  //------------------------------
+//------------------------------------------
   // 言語設定
-  const lang = getLang() // 言語設定取得
-  //------------------------------
+  const lang = COMMON.getLang() // 言語設定取得
+//------------------------------------------
   // ダイアログ フラグ
-  const [signUpFlag, setSignUpFlag] = useRecoilState(signUpFlagState)
+  const [signUpFlag, setSignUpFlag] = RCL.useRecoilState(signUpFlagState)
   const toggleSignUpFlag = () => setSignUpFlag(!signUpFlag)
 
   return (
-    <MUIDialog className='SignUpDialog' open={signUpFlag}>
-      <MUIDialogTitle>{dialogTitle[lang]}</MUIDialogTitle>
-      <MUIDialogContent>
-        <MUIDialogContentText sx={styleContentText}>
-          {dialogText[lang]}
-        </MUIDialogContentText>
-        <MUITextField sx={styleTextField} autoFocus label={"Group Name"} variant="standard" />
-      </MUIDialogContent>
-      <MUIDialogActions>
-        <MUIButton onClick={toggleSignUpFlag}>{cancelLabel[lang]/* キャンセル */}</MUIButton>
-        <MUIButton onClick={toggleSignUpFlag}>{okLabel[lang]/* 決定 */}</MUIButton>
-      </MUIDialogActions>
-    </MUIDialog>
+    <MUI.Dialog className='SignUpDialog' open={signUpFlag}>
+      <MUI.DialogTitle>{MSG.dialogTitle[lang]}</MUI.DialogTitle>
+      <MUI.DialogContent>
+        <MUI.DialogContentText sx={styleContentText}>
+          {MSG.dialogText[lang]/* 説明文 */}
+        </MUI.DialogContentText>
+        <MUI.TextField
+          label={"Group Name"} variant="standard"
+          autoFocus sx={styleTextField}
+        />
+      </MUI.DialogContent>
+      <MUI.DialogActions>
+        <MUI.Button onClick={toggleSignUpFlag}>
+          {MSG.cancelLabel[lang]/* キャンセル */}
+        </MUI.Button>
+        <MUI.Button onClick={toggleSignUpFlag}>
+          {MSG.okLabel[lang]/* 決定 */}
+        </MUI.Button>
+      </MUI.DialogActions>
+    </MUI.Dialog>
   )
 }
 
-//===================================================|0
+//=============================================================|0
 //                    Export
-//==========================================|2022_05_23
+//====================================================|2022_05_23
 export { SignUpDialog, signUpFlagState }
