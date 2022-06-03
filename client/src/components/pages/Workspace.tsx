6fd8bc2685e1b70a6ce41bb367780b3adfb79d7c
yano-kentaro <ynkn0829@gmail.com>
2022-06-03 22:52:39 +0900

    コンポーネント表示

--- client/src/components/pages/Workspace.tsx
+++ client/src/components/pages/Workspace.tsx
@@ -22,6 +22,9 @@ import { styled } from '@mui/material/styles'
 //------------------------------------------
 // Components
 import * as MUI from '@mui/material'
+import Header from 'src/components/layouts/workspace/Header'
+import Body from 'src/components/layouts/workspace/Body'
+import Footer from 'src/components/layouts/workspace/Footer'
 
 //------------------------------------------
 // Functions
@@ -48,7 +51,10 @@ function Workspace() {
 
   return (
     <MUI.Box className="Workspace">
-      <h1>test</h1>
+      <Header />
+      <MUI.Box className="h-16" />
+      <Body />
+      <Footer />
     </MUI.Box>
   )
 }
